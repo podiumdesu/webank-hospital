521f8f3dd387e48641779b792366799eb3f0a579
podiumdesu <wwhmomo@gmail.com>
2021-09-20 02:26:42 +0800

    Add: new style for width-vw

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'screen-1/4': '25vw',
+      },
+      height: {
+        'w-screen-1/4': '25vw'
+      }
+    },
   },
   variants: {
     extend: {},
