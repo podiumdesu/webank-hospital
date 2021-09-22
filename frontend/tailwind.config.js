abdab53537850e8f4c5aa453ab790ad842013f05
podiumdesu <wwhmomo@gmail.com>
2021-09-22 17:51:10 +0800

    Fix: remove textColor into extend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,17 +3,17 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    textColor: {
-      'light-black': '#707070',
-      'dark-black': '#252517',
-      '6178EE': '#6178EE'
-    },
     extend: {
       width: {
         'screen-1/4': '25vw',
       },
       height: {
         'w-screen-1/4': '25vw'
+      },
+      textColor: {
+        'light-black': '#707070',
+        'dark-black': '#252517',
+        '6178EE': '#6178EE'
       },
     },
   },
