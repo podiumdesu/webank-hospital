e19bb5152c526d54ff6cd85f1e0f4e15d2d24685
podiumdesu <wwhmomo@gmail.com>
2021-09-22 17:36:51 +0800

    Add: textColor configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,13 +3,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    textColor: {
+      'light-black': '#707070',
+      'dark-black': '#252517',
+      '6178EE': '#6178EE'
+    },
     extend: {
       width: {
         'screen-1/4': '25vw',
       },
       height: {
         'w-screen-1/4': '25vw'
-      }
+      },
     },
   },
   variants: {
