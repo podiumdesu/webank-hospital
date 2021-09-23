7ac19d3a7d817d874b384aaaa142a58b8572e1ed
podiumdesu <wwhmomo@gmail.com>
2021-09-24 02:32:07 +0800

    Refactor: record.map

--- frontend/src/pages/AuthData/all.jsx
+++ frontend/src/pages/AuthData/all.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { WingBlank, Icon } from 'antd-mobile';
 import { RecordCard, CardContainer } from '@/components/RecordCard';
 import { Link } from 'react-router-dom'
+import allMedicalRecord from '@/config/medicalRecord.json'
+import allPhysicalExamRecord from '@/config/physicalExam'
+
+const allRecord = allMedicalRecord.concat(allPhysicalExamRecord)
 
 export default () => {
     return (
         <WingBlank>
-            <RecordCard time={Date.now()} title='鸡西市中医医院 外科' description='诊断意见:阑尾炎' attachment='附件:血液检查单' />
-            <RecordCard time={Date.now()} title='宝钢医院 呼吸科' description='诊断意见:感冒' />
             <CardContainer
                 left={
                     <div className='flex-1'>
@@ -20,6 +22,12 @@ export default () => {
                     </div>
                 }
             />
+            {
+                allRecord.map((i, _idx) => (
+                    <RecordCard time={i.time} title={i.title} description={i.description} 
+                                attachment={i.attachment} key={_idx}></RecordCard>
+                ))
+            }
         </WingBlank>
     )
 }
