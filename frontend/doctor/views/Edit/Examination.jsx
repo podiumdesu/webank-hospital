a330eb0d57c9d4796008ef4cc5d36166f8baccc9
winderica <winderica@gmail.com>
2021-09-24 19:32:31 +0800

    Update: encrypt and upload attachments

--- frontend/doctor/views/Edit/Examination.jsx
+++ frontend/doctor/views/Edit/Examination.jsx
@@ -5,6 +5,8 @@ import { Upload } from '$/components/Textfields/Upload';
 import { Add, Delete, DeleteForever } from '@mui/icons-material';
 import { Table } from '$/components/Table';
 import React from 'react';
+import { AES } from '#/utils/aes';
+import { add } from '#/utils/ipfs';
 
 export const Examination = ({ attachments, setAttachments }) => {
     const {
@@ -17,8 +19,20 @@ export const Examination = ({ attachments, setAttachments }) => {
             attachment: null
         }
     });
-    const onSubmit = ({ attachment }) => {
-        setAttachments((attachments) => [...attachments, [attachment?.[0]?.name ?? '', randomHexString(16)]]);
+    const onSubmit = async ({ attachment }) => {
+        if (attachment?.length !== 1) {
+            return;
+        }
+        const dk = randomHexString(64);
+        const aes = new AES(await AES.convertKey(dk));
+        const data = await new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = () => reject(reader.error);
+            reader.readAsArrayBuffer(attachment[0]);
+        });
+        const { cid } = await add(JSON.stringify([await aes.encrypt(data, ''), aes.iv]));
+        setAttachments((attachments) => [...attachments, [attachment[0].name ?? '', cid.toString(), dk]]);
         reset({ attachment: null });
     };
     return (
@@ -44,11 +58,12 @@ export const Examination = ({ attachments, setAttachments }) => {
                     '附件名',
                     'CID',
                 ]}
-                rows={attachments.map((attachment, i) => [
+                rows={attachments.map(([name, cid], i) => [
                     <IconButton onClick={() => setAttachments((attachments) => attachments.filter((_, j) => i !== j))}>
                         <Delete />
                     </IconButton>,
-                    ...attachment
+                    name,
+                    cid,
                 ])}
             />
         </Paper>
