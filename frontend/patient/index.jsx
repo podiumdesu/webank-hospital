7d4268db52ed6336151f40358ea0bdabbd61715b
winderica <winderica@gmail.com>
2021-09-25 23:58:20 +0800

    Fix: use `HashRouter` instead

--- frontend/patient/index.jsx
+++ frontend/patient/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Routes } from './routes'
 import 'antd-mobile/dist/antd-mobile.css';
 import '@/styles/main.css';
