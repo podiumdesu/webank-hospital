22c62208d23e856d0b13290c28440ebce7dd30a6
winderica <winderica@gmail.com>
2021-09-26 18:22:01 +0800

    Fix: ensure `useLocation` is always called

--- frontend/patient/components/Header.jsx
+++ frontend/patient/components/Header.jsx
@@ -8,11 +8,12 @@ import { routes } from '@/routes';
 import { NavBar, Icon } from 'antd-mobile'
 
 export const Header = () => {
-    const [{ route: { title }, pathname }] = matchRoutes(routes, useLocation());
+    const location = useLocation();
+    const [{ route: { title }, pathname }] = matchRoutes(routes, location);
     return (
         <NavBar
             mode="light"
-            icon={pathname === '/' ? undefined : <Link to={`${useLocation().pathname}/../`}><Icon type="left" className='text-black' /></Link>}
+            icon={pathname === '/' ? undefined : <Link to={`${location.pathname}/../`}><Icon type="left" className='text-black' /></Link>}
         >
             {title}
         </NavBar>
