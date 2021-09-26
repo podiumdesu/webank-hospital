99ce8f421b86556b7a99b5702f940732e32826af
winderica <winderica@gmail.com>
2021-09-26 18:21:32 +0800

    Fix: encode hmac result in hex

--- frontend/common/utils/hmac.js
+++ frontend/common/utils/hmac.js
@@ -4,7 +4,7 @@ export const hmac = async (
     data,
     key,
     inputEncoding = 'utf-8',
-    outputEncoding = 'base64'
+    outputEncoding = 'hex'
 ) => {
     const result = await crypto.subtle.sign('HMAC', key, toUint8Array(data, inputEncoding));
     return fromUint8Array(new Uint8Array(result), outputEncoding);
