e726378b9699b78adc50597c3fbb3c20a35acea9
winderica <winderica@gmail.com>
2021-09-30 15:47:39 +0800

    Update: `deriveKeyFromPassword` now only returns `CryptoKey`

--- frontend/common/utils/kdf.js
+++ frontend/common/utils/kdf.js
@@ -1,5 +1,3 @@
-import { fromUint8Array } from './codec';
-
 export const deriveKeyFromPassword = async (password, salt) => {
     const key = await crypto.subtle.importKey(
         'raw',
@@ -8,37 +6,22 @@ export const deriveKeyFromPassword = async (password, salt) => {
         false,
         ['deriveBits', 'deriveKey']
     );
-    const algorithm ={
+    const algorithm = {
         name: 'PBKDF2',
         salt,
         iterations: 100000,
         hash: 'SHA-256',
     };
-    return {
-        hmac: await crypto.subtle.deriveKey(
-            algorithm,
-            key,
-            {
-                name: 'HMAC',
-                hash: {
-                    name: 'SHA-256',
-                },
+    return await crypto.subtle.deriveKey(
+        algorithm,
+        key,
+        {
+            name: 'HMAC',
+            hash: {
+                name: 'SHA-256',
             },
-            true,
-            ['sign', 'verify']
-        ),
-        raw: fromUint8Array(new Uint8Array(await crypto.subtle.exportKey(
-            'raw',
-            await crypto.subtle.deriveKey(
-                algorithm,
-                key,
-                {
-                    name: 'AES-GCM',
-                    length: 256,
-                },
-                true,
-                ['encrypt']
-            )
-        )), 'hex'),
-    };
+        },
+        true,
+        ['sign', 'verify']
+    );
 };
