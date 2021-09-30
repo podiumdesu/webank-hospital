7eaaac70d303ec38a67bab073d611ca462541770
winderica <winderica@gmail.com>
2021-09-30 13:54:25 +0800

    Fix: handle navigation in a better way

--- frontend/patient/components/Header.jsx
+++ frontend/patient/components/Header.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import {
     useLocation,
     matchRoutes,
-    Link
+    useNavigate,
 } from "react-router-dom";
 import { routes } from '@/routes';
 import { NavBar, Icon } from 'antd-mobile'
 
 export const Header = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const [{ route: { title }, pathname }] = matchRoutes(routes, location);
     return (
         <NavBar
             mode="light"
-            icon={pathname === '/' ? undefined : <Link to={`${location.pathname}/../`}><Icon type="left" className='text-black' /></Link>}
+            icon={pathname === '/' ? undefined : <Icon
+                type="left"
+                onClick={() => {
+                    navigate(history.length > 1 ? -1 : `${location.pathname}/../`);
+                }}
+                className='text-black'
+            />}
         >
             {title}
         </NavBar>
