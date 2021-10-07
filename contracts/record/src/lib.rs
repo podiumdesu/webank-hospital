d48daef150520288182620ff7103a9de5c165685
winderica <winderica@gmail.com>
2021-10-08 00:10:07 +0800

    Update: support getting timestamp

--- contracts/record/src/lib.rs
+++ contracts/record/src/lib.rs
@@ -16,28 +16,35 @@ mod medical_record {
     use super::{pre::*};
     #[liquid(storage)]
     struct MedicalRecord {
-        keys: storage::Mapping<String, [String; 2]>,
+        cas: storage::Mapping<String, [String; 2]>,
+        timestamps: storage::Mapping<String, timestamp>,
     }
 
     #[liquid(methods)]
     impl MedicalRecord {
         pub fn new(&mut self) {
-            self.keys.initialize();
+            self.cas.initialize();
+            self.timestamps.initialize();
         }
 
         pub fn get(&self, id: String) -> [String; 2] {
-            self.keys.get(&id).unwrap().clone()
+            self.cas.get(&id).unwrap().clone()
+        }
+
+        pub fn get_timestamp(&self, id: String) -> timestamp {
+            self.timestamps.get(&id).unwrap().clone()
         }
 
         pub fn set(&mut self, id: String, key: [String; 2]) {
-            if !self.keys.contains_key(&id) {
-                self.keys.insert(&id, key);
+            if !self.cas.contains_key(&id) {
+                self.cas.insert(&id, key);
+                self.timestamps.insert(&id, self.env().now());
             }
         }
 
         pub fn re_encrypt(&self, id: String, rk: String) -> [String; 2] {
             let pre = PRE::at("0x5007".parse().unwrap());
-            let key = self.keys.get(&id).unwrap();
+            let key = self.cas.get(&id).unwrap();
             [key[0].clone(), pre.reEncrypt(key[1].clone(), rk).unwrap().clone()]
         }
     }
