cb4a917b26e9fa1bbc6457b27319d4b73435b0e8
winderica <winderica@gmail.com>
2021-10-09 17:56:58 +0800

    Update: create idb store for examinations

--- frontend/patient/stores/idb.js
+++ frontend/patient/stores/idb.js
@@ -2,12 +2,14 @@ import { openDB } from 'idb';
 
 export const stores = {
     record: 'record',
+    examination: 'examination',
     metadata: 'metadata',
 }
 
 export const db = await openDB('patient', 1, {
   upgrade(db) {
     db.createObjectStore(stores.record);
+    db.createObjectStore(stores.examination);
     db.createObjectStore(stores.metadata);
   },
 });
