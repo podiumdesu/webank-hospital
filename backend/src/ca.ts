55b8a63731ca557cf5146f2548b69491a2a4bf00
winderica <winderica@gmail.com>
2021-10-09 22:51:25 +0800

    Update: more members joined the blockchain

--- backend/src/ca.ts
+++ backend/src/ca.ts
@@ -9,10 +9,17 @@ const caConfig = new Configuration({
 
 const web3j = new Web3jService(caConfig);
 
+const members = [
+    '0x47f76a5e5ad8CF4E6D3E7c854678Bc2DfBb5f2A2',
+    '0xF12baAAB2b949497ce05C986269701EbCe866A5C',
+    '0x7B59b2cC8b3d7712Cd67D92D134385277872EE80',
+    '0x97E042b532a8796eecF184D91c7C6fc83D8ca774',
+];
+
 (async () => {
-    await web3j.sendRawTransaction(
+    await Promise.all(members.map((member) => web3j.sendRawTransaction(
         addresses.ca,
         'function issue(address member) public',
-        ['0x47f76a5e5ad8CF4E6D3E7c854678Bc2DfBb5f2A2']
-    );
+        [member]
+    )));
 })();
