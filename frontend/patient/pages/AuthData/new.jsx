48b6c13b08d317b175b47ead687266028ce37b59
winderica <winderica@gmail.com>
2021-10-09 18:03:28 +0800

    Update: store data to different stores accordingly

--- frontend/patient/pages/AuthData/new.jsx
+++ frontend/patient/pages/AuthData/new.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { toDataURL } from 'qrcode';
-import { keyGen, G1, Fr, decrypt } from '#/utils/pre';
+import { decrypt, Fr, G1, keyGen } from '#/utils/pre';
 import { Scanner } from '#/components/Scanner';
 import { g, h } from '#/constants';
-import { Toast, Steps, Button, Form } from 'antd-mobile';
+import { Button, Form, Steps, Toast } from 'antd-mobile';
 import { CheckOutline, CloseOutline } from 'antd-mobile-icons';
 import { CID } from 'multiformats/cid';
 import { hmac } from '#/utils/hmac';
@@ -13,6 +13,8 @@ import { api } from '@/api';
 import { db, stores } from '@/stores/idb';
 import { useMobxStore } from '@/stores/mobx';
 import { useNavigate } from 'react-router-dom';
+import { keccak_256 } from 'js-sha3';
+import { hexToUint8Array } from '#/utils/codec';
 
 const { Step } = Steps;
 
@@ -24,6 +26,7 @@ export default () => {
     const [ca, setCa] = useState();
     const [sk, setSk] = useState();
     const [data, setData] = useState();
+    const [valid, setValid] = useState(false);
     const store = useMobxStore();
     useEffect(() => {
         const { pk, sk } = keyGen(g);
@@ -61,7 +64,14 @@ export default () => {
             // display the data and wait user for approval
             {
                 const { data, signature, recid } = JSON.parse(await aes.decrypt(buffer.slice(12), ''));
-                console.log(signature, recid);
+                setValid(await api.verify(
+                    data.address,
+                    keccak_256.update(JSON.stringify(data)).array(),
+                    recid + 27,
+                    hexToUint8Array(signature.slice(0, 64)),
+                    hexToUint8Array(signature.slice(64)),
+                    Date.now(),
+                ));
                 setData(data);
             }
 
@@ -78,31 +88,46 @@ export default () => {
             });
             return false;
         }
-    }
+    };
     const handleUpload = async () => {
         try {
             await api.setRecord(await hmac(cid.bytes, await store.hk, ''), ca.map(i => i.serializeToHexStr()));
-            await db.put(stores.record, {
-                time: new Date(data.time),
-                title: `${data.hospital} ${data.department}`,
-                description: data.diagnosis,
-                attachments: data.attachments.map(([name]) => name).join(', '),
-                sk: sk.serialize()
-            }, cid.bytes);
+            switch (data.type) {
+                case 'record':
+                    await db.put(stores.record, {
+                        time: new Date(data.time),
+                        title: `${data.hospital} ${data.department}`,
+                        description: data.diagnosis,
+                        attachments: data.attachments.map(({ name }) => name).join(', '),
+                        sk: sk.serialize()
+                    }, cid.bytes);
+                    break;
+                case 'examination':
+                    await db.put(stores.examination, {
+                        time: new Date(data.time),
+                        title: `${data.hospital}`,
+                        description: data.project,
+                        attachments: data.attachments.map(({ name }) => name).join(', '),
+                        sk: sk.serialize()
+                    }, cid.bytes);
+                    break;
+                default:
+                    throw new Error(`Unknown type ${data.type}`);
+            }
             Toast.show({
                 icon: <CheckOutline className='mx-auto' />,
                 content: '提交成功',
                 afterClose: () => {
                     navigate('/');
                 },
-            })
+            });
         } catch (e) {
             Toast.show({
                 icon: <CloseOutline className='mx-auto' />,
                 content: e.message,
-            })
+            });
         }
-    }
+    };
     return (
         <div className='px-4'>
             <Steps current={step}>
@@ -125,18 +150,21 @@ export default () => {
                         layout='horizontal'
                         footer={<Button className='mt-2' block onClick={handleUpload}>数据上链</Button>}
                     >
+                        <Form.Item label='时间'>
+                            {data ? new Date(data.time).toLocaleString('zh-CN', { dateStyle: 'long', timeStyle: 'medium' }) : ''}
+                        </Form.Item>
                         <Form.Item label='医院'>
                             {data?.hospital}
                         </Form.Item>
                         <Form.Item label='医生'>
                             {data?.doctor}
                         </Form.Item>
-                        <Form.Item label='诊断意见'>
-                            {data?.diagnosis}
+                        <Form.Item label='签名'>
+                            {valid ? '有效' : '无效'}
                         </Form.Item>
                     </Form>,
                 ][step]
             }
         </div>
-    )
+    );
 }
