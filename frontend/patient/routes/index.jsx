ac31dda08af7ad8ac2920f631b1538c8f8944f6c
winderica <winderica@gmail.com>
2021-10-09 18:20:59 +0800

    Update: new logo

--- frontend/patient/routes/index.jsx
+++ frontend/patient/routes/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Home from '@/pages/MainPage';
-import { useRoutes, Outlet } from 'react-router-dom';
-import loadable from '@loadable/component'
+import { Outlet, useRoutes } from 'react-router-dom';
+import loadable from '@loadable/component';
+import logoIcon from '@/images/logo/logoIcon.png';
+import logoText from '@/images/logo/logoText.png';
 
 const MedicalCard = loadable(() => import('@/pages/MedicalCard'));
 const MedicalRecord = loadable(() => import('@/pages/MedicalRecord'));
@@ -14,70 +16,74 @@ const AllAuthData = loadable(() => import('@/pages/AuthData/all'));
 const DetailedAuthData = loadable(() => import('@/pages/AuthData/detail'));
 
 export const routes = [
-  {
-    path: '/medicalCard',
-    element: <MedicalCard />,
-    title: '电子就诊卡'
-  },
-  {
-    path: '/medicalRecord',
-    element: <Outlet />,
-    title: '电子病历',
-    children: [
-      {
-        path: ':cid',
-        element: <DetailedMedicalRecord />,
-      },
-      {
-        path: '',
-        element: <MedicalRecord />,
-      }
-    ]
-  },
-  {
-    path: '/physicalExamData',
-    element: <Outlet />,
-    title: '体检数据',
-    children: [
-      {
-        path: ':cid',
-        element: <DetailedMedicalExamination />,
-      },
-      {
-        path: '',
-        element: <MedicalExamination />,
-      }
-    ]
-  },
-  {
-    path: '/reportProgress',
-    element: <ReportProgress />,
-    title: '报告进度'
-  },
-  {
-    path: '/authData',
-    element: <Outlet />,
-    title: '授权数据',
-    children: [
-      {
-        path: ':cid',
-        element: <DetailedAuthData />
-      },
-      {
-        path: 'new',
-        element: <NewAuthData />,
-      },
-      {
-        path: '',
-        element: <AllAuthData />,
-      },
-    ]
-  },
-  {
-    path: '*',
-    element: <Home />,
-    title: <>链<span className='font-bold text-blue-400'>＋</span>医疗服务平台</>
-  },
+    {
+        path: '/medicalCard',
+        element: <MedicalCard />,
+        title: '电子就诊卡'
+    },
+    {
+        path: '/medicalRecord',
+        element: <Outlet />,
+        title: '电子病历',
+        children: [
+            {
+                path: ':cid',
+                element: <DetailedMedicalRecord />,
+            },
+            {
+                path: '',
+                element: <MedicalRecord />,
+            }
+        ]
+    },
+    {
+        path: '/physicalExamData',
+        element: <Outlet />,
+        title: '体检数据',
+        children: [
+            {
+                path: ':cid',
+                element: <DetailedMedicalExamination />,
+            },
+            {
+                path: '',
+                element: <MedicalExamination />,
+            }
+        ]
+    },
+    {
+        path: '/reportProgress',
+        element: <ReportProgress />,
+        title: '报告进度'
+    },
+    {
+        path: '/authData',
+        element: <Outlet />,
+        title: '授权数据',
+        children: [
+            {
+                path: ':cid',
+                element: <DetailedAuthData />
+            },
+            {
+                path: 'new',
+                element: <NewAuthData />,
+            },
+            {
+                path: '',
+                element: <AllAuthData />,
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <Home />,
+        title: <div className='flex justify-center items-center'>
+          <img src={logoIcon} alt='' className='h-4' />
+          <img src={logoText} alt='' className='h-4 mx-1' />
+          服务平台
+        </div>
+    },
 ];
 
 export const Routes = () => useRoutes(routes);
