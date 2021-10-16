670a8e89997a568b5877b87e263dc8a8cb139a56
rika <winderica@gmail.com>
2021-10-16 21:58:56 +0800

    Fix: finally building works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,17 +9,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@motrix/nat-api': './lib/empty.js',
-      'randombytes': './lib/randombytes.js',
-      'electron-fetch': './lib/empty.js',
-      'mortice': './lib/mortice.js',
-      'readable-stream': './lib/readable-stream/index.js',
-      'levelup': './lib/levelup.js',
-      'level-js': './lib/level-js.js',
-      'ipfs-utils/src/env': './lib/ipfs-utils/env.js',
-      'ipfs-utils/src/supports': './lib/ipfs-utils/supports.js',
-      'ipfs-utils/src': './lib/ipfs-utils',
-
       '@': resolve(__dirname, 'patient'),
       '#': resolve(__dirname, 'common'),
       '$': resolve(__dirname, 'doctor'),
