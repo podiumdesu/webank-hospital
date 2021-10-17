3fd73a47b520e0a7c446d2a0e3f56b5f7cdf4b67
rika <winderica@gmail.com>
2021-10-17 19:30:28 +0800

    Update: prefer environment variables

--- frontend/common/constants/index.js
+++ frontend/common/constants/index.js
@@ -1,4 +1,4 @@
 import { generatorGen } from '#/utils/pre';
 
 export const { g, h } = generatorGen('foo', 'bar');
-export const ENDPOINT = 'http://localhost:5000';
+export const ENDPOINT = import.meta.env.VITE_ENDPOINT ?? 'http://localhost:5000';
