60959032eaa07984b7a0aca77d421e5908dac6dc
winderica <winderica@gmail.com>
2021-10-19 21:59:36 +0800

    Update: @vitejs/plugin-react-refresh -> @vitejs/plugin-react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   define: {
     'process.env.DUMP_SESSION_KEYS': '""'
   },
