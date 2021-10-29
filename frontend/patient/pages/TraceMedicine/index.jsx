6c32b68d43e88002b747d5f359b0c1f4108aa050
rika <winderica@gmail.com>
2021-10-29 21:17:41 +0800

    Update: improve traceability

--- frontend/patient/pages/TraceMedicine/index.jsx
+++ frontend/patient/pages/TraceMedicine/index.jsx
@@ -1,57 +1,70 @@
 import React, { useState } from 'react';
 import { api } from '@/api';
-import { base64ToUint8Array, hexToUint8Array, uint8ArrayToHex } from '#/utils/codec';
+import { base64ToUint8Array, uint8ArrayToHex } from '#/utils/codec';
 import { hash } from '#/utils/rescue';
 import { AES } from '#/utils/aes';
-import { Button, Form, Steps, Toast } from 'antd-mobile';
+import { sha256 } from '#/utils/sha';
+import { Steps, Toast } from 'antd-mobile';
 import { Scanner } from '#/components/Scanner';
 import { CheckOutline, CloseOutline } from 'antd-mobile-icons';
-import { keccak_256 } from 'js-sha3';
-import { useNavigate } from 'react-router-dom';
+import { deserializeHexStrToPublicKey, deserializeHexStrToSignature, multiPkAggregate, multiVerify } from '#/utils/bls';
 
 const { Step } = Steps;
 
+const verify = (nodes, signature) => {
+    if (!nodes.length || !signature.length) {
+        return true;
+    }
+    const pks = [];
+    for (let i = 1; i < nodes.length; i++) {
+        pks.push(multiPkAggregate([nodes[i - 1].pk, nodes[i].pk]));
+    }
+    const lastNode = nodes[nodes.length - 1];
+    return multiVerify(signature[0], pks, nodes.slice(0, -1).map(({ digest }) => digest)) && lastNode.pk.verify(signature[1], lastNode.digest);
+}
+
 export default () => {
-    const navigate = useNavigate();
-    const [trace, setTrace] = useState([]);
+    const [nodes, setNodes] = useState([]);
+    const [signature, setSignature] = useState([]);
     const [step, setStep] = useState(0);
     const handleData = async (data) => {
-        if (data.length >= 32) {
+        try {
+            if (data.length < 32) {
+                throw new Error('Invalid tracing code');
+            }
             const tracingCode = data.slice(0, 32);
-            const id = hash(new Uint8Array(32).fill(0), tracingCode);
-            const trace = await api.getTrace(uint8ArrayToHex(id));
+            const id = uint8ArrayToHex(hash(new Uint8Array(32).fill(0), tracingCode));
+            const trace = await api.getTraceNodes(id);
             Toast.show({
                 icon: <CheckOutline className='mx-auto' />,
                 content: '扫描成功',
             });
-            setTrace(await Promise.all(trace.map(async ([item, timestamp]) => {
+            setNodes(await Promise.all(trace.map(async ([item, timestamp]) => {
                 const buffer = base64ToUint8Array(item);
                 const aes = new AES(await AES.convertKey(uint8ArrayToHex(tracingCode)), buffer.slice(0, 12));
-                const { data, signature, recid } = JSON.parse(await aes.decrypt(buffer.slice(12), ''));
+                const data = JSON.parse(await aes.decrypt(buffer.slice(12), ''));
+                const pk = deserializeHexStrToPublicKey(await api.getPK(data.address));
                 return {
                     data,
-                    valid: await api.verify(
-                        data.address,
-                        keccak_256.update(JSON.stringify(data)).array(),
-                        recid + 27,
-                        hexToUint8Array(signature.slice(0, 64)),
-                        hexToUint8Array(signature.slice(64)),
-                        timestamp,
-                    )
+                    pk,
+                    digest: await sha256(buffer),
+                    timestamp,
                 };
             })));
+            setSignature((await api.getTraceSignature(id)).map(deserializeHexStrToSignature));
             setStep(1);
             return true;
-        } else {
+        } catch (e) {
             Toast.show({
                 icon: <CloseOutline className='mx-auto' />,
-                content: 'Invalid tracing code',
+                content: e.message,
             });
             return false;
         }
     };
+    console.log(signature);
     return (
-        <div className='px-4'>
+        <div className='px-4 flex-1 bg-[#C8DBFF33]'>
             <Steps current={step}>
                 <Step title='扫溯源码' />
                 <Step title='供应链展示' />
@@ -64,22 +77,34 @@ export default () => {
                             <Scanner onData={handleData} className='rounded-xl' />
                         </div>
                     </div>,
-                    <div className="flex flex-col items-center">
-                        {trace.map(({ data, valid }, index) => (
+                    <div className='flex flex-col gap-4 bg-white w-full p-4 rounded-xl relative'>
+                        <p className='absolute bottom-0 right-0 rounded-br-lg rounded-tl-lg p-1 px-3 z-20 text-xs text-dark-black bg-[#FBCD6F]'>
+                            签名验证{verify(nodes, signature) ? '成功' : '失败'}
+                        </p>
+                        {nodes.map(({ data: { address, ...other } }, index) => (
                             <div key={index}>
-                                <p>供应链节点{index + 1}: 签名验证{valid ? '成功' : '失败'}</p>
-                                <Form
-                                    layout='horizontal'
-                                >
-                                    {Object.entries(data).map(([key, value]) => (
-                                        <Form.Item label={key} key={key}>
-                                            {value}
-                                        </Form.Item>
-                                    ))}
-                                </Form>
+                                <div className='flex justify-between gap-3 text-[#60A2F8]'>
+                                    <div className='flex flex-col justify-center'>
+                                        <p className='text-lg font-bold w-max'>供应链节点{index + 1}</p>
+                                    </div>
+                                    <div className='flex flex-col justify-center text-right min-w-0'>
+                                        <p className='text-xs truncate'>{address}</p>
+                                    </div>
+                                </div>
+                                <table className='text-xs mt-2'>
+                                    <tbody>
+                                        {
+                                            Object.entries(other).map(([k, v]) => (
+                                                <tr key={k} className='my-2'>
+                                                    <th className='text-left font-normal pr-3 py-1 text-light-black w-20'>{k}:</th>
+                                                    <td className='text-[#25261F]'>{v}</td>
+                                                </tr>
+                                            ))
+                                        }
+                                    </tbody>
+                                </table>
                             </div>
                         ))}
-                        <Button className='text-[#61A1F8] border-[#61A1F8]' onClick={() => navigate('/')}>完成</Button>
                     </div>
                 ][step]
             }
--- frontend/supplyChain/index.jsx
+++ frontend/supplyChain/index.jsx
@@ -1,57 +1,142 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { render } from 'react-dom';
 import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
     Alert,
+    Avatar,
     Button,
+    Collapse,
     CssBaseline,
-    MenuItem,
+    Dialog,
+    DialogTitle,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Paper,
     Snackbar,
     Stack,
     Step,
     StepContent,
     StepLabel,
     Stepper,
-    TextField,
+    Table as MuiTable,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
     ThemeProvider,
-    Typography
 } from '@mui/material';
-import { ExpandMore } from '@mui/icons-material';
+import { red, blue, green } from '@mui/material/colors';
+import { ExpandMore, Person } from '@mui/icons-material';
 import { Input } from '$/components/Textfields/Input';
 import { useForm } from 'react-hook-form';
 import { LocalizationProvider } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import { keccak_256 } from 'js-sha3';
 import { base64ToUint8Array, hexToUint8Array, uint8ArrayToBase64, uint8ArrayToHex } from '#/utils/codec';
-import { ecdsaSign } from 'secp256k1';
 import { AES } from '#/utils/aes';
+import { sha256 } from '#/utils/sha';
+import { SecretKey, multiSigAggregate, aggregate, Signature, deserializeHexStrToPublicKey, deserializeHexStrToSignature, multiPkAggregate, multiVerify } from '#/utils/bls';
 import { hash, prove } from '#/utils/rescue';
 import { SimpleTable, Table } from '$/components/Table';
 import { API, ClientConfig } from '#/api/v2';
 import { Scanner } from '#/components/Scanner';
-import { api } from '@/api'; // TODO: shouldn't import api from @
 
 const getDefaultValue = () => ({
     name: '',
     value: '',
 });
 
-const identities = [
-    { name: '生产商', privateKey: 'f7aa567e075df14664637e96de0fb3a44a77e400e1eecb3392a2055b0c190a29' },
-    { name: '运输公司', privateKey: '6cc9e434e5b4ecc62c2f66deabd87487127c0a664214d14b162f39ce9e564b5e' },
-    { name: '药房', privateKey: 'caaad6e117b5f1ca4cfd08217a0a1343342ce96769ccaf93b9ea3c1026ceb986' },
-];
+const identities = {
+    'f7aa567e075df14664637e96de0fb3a44a77e400e1eecb3392a2055b0c190a29': {
+        name: '生产商',
+        color: red,
+    },
+    '6cc9e434e5b4ecc62c2f66deabd87487127c0a664214d14b162f39ce9e564b5e': {
+        name: '运输公司',
+        color: blue,
+    },
+    'caaad6e117b5f1ca4cfd08217a0a1343342ce96769ccaf93b9ea3c1026ceb986': {
+        name: '药房',
+        color: green,
+    },
+};
+
+const verify = (nodes, signature, sk) => {
+    if (!nodes.length || !signature.length) {
+        return true;
+    }
+    const pks = [];
+    for (let i = 1; i < nodes.length; i++) {
+        pks.push(multiPkAggregate([nodes[i - 1].pk, nodes[i].pk]));
+    }
+    const lastNode = nodes[nodes.length - 1];
+    const pk = [lastNode.pk, sk.getPublicKey()];
+    pks.push(multiPkAggregate(pk));
+    return multiVerify(aggregate([signature[0], multiSigAggregate([signature[1], sk.sign(lastNode.digest)], pk)]), pks, nodes.map(({ digest }) => digest));
+}
+
+const CollapsibleTable = ({ nodes }) => {
+    const [open, setOpen] = useState(new Set());
+    return nodes.length ? (
+        <TableContainer component={Paper}>
+            <MuiTable size='small'>
+                <TableHead>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell>节点序号</TableCell>
+                        <TableCell>地址</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {nodes.map(({ data: { address, ...other } }, index) => (
+                        <React.Fragment>
+                            <TableRow>
+                                <TableCell>
+                                    <IconButton
+                                        size="small"
+                                        onClick={() => setOpen((open) => {
+                                            open = new Set(open);
+                                            open.has(index) ? open.delete(index) : open.add(index);
+                                            return open;
+                                        })}
+                                    >
+                                        <ExpandMore sx={{
+                                            transition: (theme) => theme.transitions.create(['transform']),
+                                            transform: open.has(index) ? 'rotate(180deg)' : undefined
+                                        }} />
+                                    </IconButton>
+                                </TableCell>
+                                <TableCell component="th" scope="row">{index + 1}</TableCell>
+                                <TableCell>{address}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell sx={{ py: 0 }} />
+                                <TableCell sx={{ py: 0 }} colSpan={2}>
+                                    <Collapse in={open.has(index)}>
+                                        <SimpleTable columns={['属性', '值']} rows={Object.entries(other)} />
+                                    </Collapse>
+                                </TableCell>
+                            </TableRow>
+                        </React.Fragment>
+                    ))}
+                </TableBody>
+            </MuiTable>
+        </TableContainer>
+    ) : null;
+}
 
 const App = () => {
     const [step, setStep] = useState(0);
     const [props, setProps] = useState([]);
-    const [privateKey, setPrivateKey] = useState(identities[0].privateKey);
+    const [privateKey, setPrivateKey] = useState('');
     const [tracingCode, setTracingCode] = useState();
     const [message, setMessage] = useState('');
-    const [trace, setTrace] = useState([]);
+    const [nodes, setNodes] = useState([]);
+    const [signature, setSignature] = useState([]);
+    const [open, setOpen] = useState(false);
     const {
         control,
         formState: { isValid },
@@ -61,6 +146,19 @@ const App = () => {
         mode: 'onChange',
         defaultValues: getDefaultValue()
     });
+
+    const sk = useMemo(() => {
+        const sk = new SecretKey();
+        sk.setHashOf(privateKey);
+        return sk;
+    }, [privateKey]);
+
+    const config = useMemo(() => new ClientConfig(privateKey), [privateKey]);
+    const api = useMemo(() => new API(config), [config]);
+
+    const verified = useMemo(() => verify(nodes, signature, sk), [signature]);
+    const lastNode = nodes[nodes.length - 1];
+
     const handleReset = () => reset(getDefaultValue());
 
     const handleClose = (event, reason) => {
@@ -70,50 +168,51 @@ const App = () => {
         setMessage('');
     };
     const handleData = async (data) => {
-        if (data.length >= 32) {
+        try {
+            if (data.length < 32) {
+                throw new Error('Invalid tracing code');
+            }
             const tracingCode = data.slice(0, 32);
             setTracingCode(tracingCode);
-            const id = hash(new Uint8Array(32).fill(0), tracingCode);
-            const trace = await api.getTrace(uint8ArrayToHex(id));
-            setTrace(await Promise.all(trace.map(async ([item, timestamp]) => {
+            const id = uint8ArrayToHex(hash(new Uint8Array(32).fill(0), tracingCode));
+            const trace = await api.getTraceNodes(id);
+            setNodes(await Promise.all(trace.map(async ([item, timestamp]) => {
                 const buffer = base64ToUint8Array(item);
                 const aes = new AES(await AES.convertKey(uint8ArrayToHex(tracingCode)), buffer.slice(0, 12));
-                const { data, signature, recid } = JSON.parse(await aes.decrypt(buffer.slice(12), ''));
+                const data = JSON.parse(await aes.decrypt(buffer.slice(12), ''));
+                const pk = deserializeHexStrToPublicKey(await api.getPK(data.address));
                 return {
                     data,
-                    signature,
-                    recid,
+                    pk,
+                    digest: await sha256(buffer),
                     timestamp,
                 };
             })));
+            setSignature((await api.getTraceSignature(id)).map(deserializeHexStrToSignature));
             setStep(1);
             return true;
-        } else {
-            setMessage('Invalid tracing code');
+        } catch (e) {
+            setMessage(e.message);
             return false;
         }
     };
     const handlePost = async () => {
         try {
-            const config = new ClientConfig(privateKey);
-            const api = new API(config);
             const aes = new AES(await AES.convertKey(uint8ArrayToHex(tracingCode)));
             const data = Object.fromEntries(props.map(({ name, value }) => [name, value]));
             data.address = config.address;
-            const {
-                signature,
-                recid
-            } = ecdsaSign(new Uint8Array(keccak_256.update(JSON.stringify(data)).arrayBuffer()), config.privateKey);
-            const c = await aes.encrypt(JSON.stringify({
-                data,
-                signature: uint8ArrayToHex(signature),
-                recid,
-            }), 'utf-8', '');
+            const c = new Uint8Array([...aes.iv, ...await aes.encrypt(JSON.stringify(data), 'utf-8', '')]);
             const blockHash = hexToUint8Array(await api.getBlockHash());
             const id = hash(new Uint8Array(32).fill(0), tracingCode);
             const digest = hash(id, blockHash);
             const proof = prove(tracingCode, blockHash, digest);
-            await api.setTrace(uint8ArrayToHex(id), uint8ArrayToBase64(new Uint8Array([...aes.iv, ...c])), proof);
+            await api.setTrace(
+                uint8ArrayToHex(id),
+                uint8ArrayToBase64(c),
+                (lastNode ? multiSigAggregate([signature[1], sk.sign(lastNode.digest)], [lastNode.pk, sk.getPublicKey()]) : new Signature()).serializeToHexStr(),
+                sk.sign(await sha256(c)).serializeToHexStr(),
+                proof
+            );
             setMessage('提交成功');
             setProps([]);
             setStep(0);
@@ -122,83 +221,93 @@ const App = () => {
         }
     };
     return (
-        <Stepper activeStep={step} orientation='vertical' sx={{ p: 1, m: 1 }}>
-            <Step>
-                <StepLabel>扫描溯源码</StepLabel>
-                <StepContent>
-                    <Scanner onData={handleData} />
-                </StepContent>
-            </Step>
-            <Step>
-                <StepLabel>数据上链</StepLabel>
-                <StepContent>
-                    {trace.map(({ data }, index) => (
-                        <Accordion key={index}>
-                            <AccordionSummary expandIcon={<ExpandMore />}>
-                                <Typography>{data.name}</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <SimpleTable columns={['属性', '值']} rows={Object.entries(data)} />
-                            </AccordionDetails>
-                        </Accordion>
+        <>
+            <Dialog onClose={() => setOpen(false)} open={open || !privateKey} fullWidth maxWidth='xs'>
+                <DialogTitle>选择身份</DialogTitle>
+                <List sx={{ pt: 0 }}>
+                    {Object.entries(identities).map(([privateKey, { name, color }]) => (
+                        <ListItem button onClick={() => {
+                            setPrivateKey(privateKey);
+                            setOpen(false);
+                        }} key={privateKey}>
+                            <ListItemAvatar>
+                                <Avatar sx={{ bgcolor: color[100], color: color[600] }}>
+                                    <Person />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={name} />
+                        </ListItem>
                     ))}
-                    <Accordion defaultExpanded={true}>
-                        <AccordionSummary expandIcon={<ExpandMore />}>
-                            <Typography>新节点</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Stack spacing={1}>
-                                <TextField
-                                    select
-                                    label='我是'
-                                    size='small'
-                                    value={privateKey}
-                                    fullWidth
-                                    onChange={({ target }) => setPrivateKey(target.value)}
-                                >
-                                    {identities.map(({ name, privateKey }) => (
-                                        <MenuItem key={name} value={privateKey}>
-                                            {name}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                                <Table
-                                    columns={Object.entries({ name: '属性', value: '值' }).map(([field, headerName]) => ({
-                                        field,
-                                        headerName,
-                                        renderEditCell: () => (
-                                            <Input
-                                                name={field}
-                                                label={headerName}
-                                                control={control}
-                                                variant='standard'
-                                                fullWidth
-                                                sx={{ mx: 1 }}
-                                            />
-                                        )
-                                    }))}
-                                    rows={props}
-                                    title='本环节数据'
-                                    deleteRow={(id) => setProps(props.filter((prop) => prop.id !== id))}
-                                    updateRow={(id) => handleSubmit((data) => {
-                                        setProps((props) => [...props, { id, ...data }]);
-                                        handleReset();
-                                    })()}
-                                    isValid={isValid}
-                                    reset={handleReset}
-                                />
-                                <Button fullWidth disabled={!props.length} onClick={handlePost}>提交</Button>
-                            </Stack>
-                        </AccordionDetails>
-                    </Accordion>
-                </StepContent>
-            </Step>
+                </List>
+            </Dialog>
             <Snackbar open={!!message} autoHideDuration={6000} onClose={handleClose}>
                 <Alert onClose={handleClose} severity='info' sx={{ width: '100%' }}>
                     {message}
                 </Alert>
             </Snackbar>
-        </Stepper>
+            {privateKey && (
+                <Paper sx={{ mx: 'auto', maxWidth: 800, p: 2, my: 1 }}>
+                    <Alert severity='info' action={<Button onClick={() => setOpen(true)}>切换身份</Button>}>我是{identities[privateKey].name}</Alert>
+                    <Stepper activeStep={step} orientation='vertical'>
+                        <Step>
+                            <StepLabel>扫描溯源码</StepLabel>
+                            <StepContent>
+                                <Scanner onData={handleData} />
+                            </StepContent>
+                        </Step>
+                        <Step>
+                            <StepLabel>供应链信息</StepLabel>
+                            <StepContent>
+                                <Stack spacing={1}>
+                                    {
+                                        lastNode ? <Alert severity={verified ? 'success' : 'error'}>验证{verified ? '成功' : '失败'}</Alert>
+                                            : <Alert severity='info'>供应链空空如也</Alert>
+                                    }
+                                    <CollapsibleTable nodes={nodes} />
+                                    <div>
+                                        <Button onClick={() => setStep(0)}>完成</Button>
+                                        <Button onClick={() => setStep(2)} disabled={nodes.find(({ data: { address } }) => address === config.address)}>新增节点</Button>
+                                    </div>
+                                </Stack>
+                            </StepContent>
+                        </Step>
+                        <Step>
+                            <StepLabel>填写信息</StepLabel>
+                            <StepContent>
+                                <Stack spacing={1}>
+                                    <Table
+                                        columns={Object.entries({ name: '属性', value: '值' }).map(([field, headerName]) => ({
+                                            field,
+                                            headerName,
+                                            renderEditCell: () => (
+                                                <Input
+                                                    name={field}
+                                                    label={headerName}
+                                                    control={control}
+                                                    variant='standard'
+                                                    fullWidth
+                                                    sx={{ mx: 1 }}
+                                                />
+                                            )
+                                        }))}
+                                        rows={props}
+                                        title='本环节数据'
+                                        deleteRow={(id) => setProps(props.filter((prop) => prop.id !== id))}
+                                        updateRow={(id) => handleSubmit((data) => {
+                                            setProps((props) => [...props, { id, ...data }]);
+                                            handleReset();
+                                        })()}
+                                        isValid={isValid}
+                                        reset={handleReset}
+                                    />
+                                    <Button disabled={!props.length} onClick={handlePost}>提交</Button>
+                                </Stack>
+                            </StepContent>
+                        </Step>
+                    </Stepper>
+                </Paper>
+            )}
+        </>
     );
 };
 
