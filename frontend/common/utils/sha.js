4c752c515c4309ee3062d7cef197c0b1e21e847d
rika <winderica@gmail.com>
2021-10-29 21:15:14 +0800

    Update: `sha256` now accepts `Uint8Array` as input and returns `Uint8Array`

--- frontend/common/utils/sha.js
+++ frontend/common/utils/sha.js
@@ -1,7 +1,3 @@
-import { uint8ArrayToHex } from './codec';
-
-const textEncoder = new TextEncoder();
-
-export const sha256 = async (message) => uint8ArrayToHex(new Uint8Array(
-    await crypto.subtle.digest('SHA-256', textEncoder.encode(message))
-));
+export const sha256 = async (message) => new Uint8Array(
+    await crypto.subtle.digest('SHA-256', message)
+);
