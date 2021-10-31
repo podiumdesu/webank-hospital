7c0e8734b6cdeaaea438ad9cf64ce905f9615fb2
rika <winderica@gmail.com>
2021-10-31 14:26:40 +0800

    Fix: make `Card` clickable

--- frontend/patient/components/Card.jsx
+++ frontend/patient/components/Card.jsx
@@ -14,25 +14,27 @@ export const CardContainer = ({ left, right, children }) => {
 
 export const Card = ({ time, title, description, attachment, to, state = null }) => {
     return (
-        <CardContainer
-            left={
-                <div className='flex-1'>
-                    <p className='text-dark-black font-medium mb-1.5'>{new Date(time).toLocaleString('zh-CN', { dateStyle: 'long' })}</p>
-                    <p className='text-dark-black font-semibold text-base mb-0.5'>{title}</p>
-                    <p className='text-6178EE'>{description}</p>
-                </div>
-            }
-            right={
-                <div className='flex items-center'>
-                    <Link to={to} state={state}><RightOutline className='text-6178EE' /></Link>
-                </div>
-            }
-        >
-            {attachment && (
-                <p className='absolute top-0 right-0 rounded-bl-lg rounded-tr-lg p-1 px-3 z-20 text-xs text-dark-black bg-[#FBCD6F]'>
-                    {attachment}
-                </p>
-            )}
-        </CardContainer>
+        <Link to={to} state={state}>
+            <CardContainer
+                left={
+                    <div className='flex-1'>
+                        <p className='text-dark-black font-medium mb-1.5'>{new Date(time).toLocaleString('zh-CN', { dateStyle: 'long' })}</p>
+                        <p className='text-dark-black font-semibold text-base mb-0.5'>{title}</p>
+                        <p className='text-6178EE'>{description}</p>
+                    </div>
+                }
+                right={
+                    <div className='flex items-center'>
+                        <RightOutline className='text-6178EE' />
+                    </div>
+                }
+            >
+                {attachment && (
+                    <p className='absolute top-0 right-0 rounded-bl-lg rounded-tr-lg p-1 px-3 z-20 text-xs text-dark-black bg-[#FBCD6F]'>
+                        {attachment}
+                    </p>
+                )}
+            </CardContainer>
+        </Link>
     )
 };
